5064bb527687683cf17caa3b329b6e40c979972a
kjokelley <kjokelley@gmail.com>
2024-11-23 18:51:55 -0800

    Created new graphs and county selector

--- app/(components)/charts/AccidentsByYear.tsx
+++ app/(components)/charts/AccidentsByYear.tsx
@@ -1,18 +1,12 @@
 
-import {CartesianGrid, Bar, BarChart, XAxis, YAxis, Tooltip, ResponsiveContainer} from "recharts"
+import {AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianAxis} from "recharts"
+
 
 type AccidentData = {
     
     data: {
-        year: number;
-        count: number;
-        //total_injuries: number | null;
-        //total_fatalities: number | null;
-        //pedestrian_accidents: number;
-        //bicycle_accidents: number;
-        //motorcycle_accidents: number;
-        //truck_accidents: number;
-        //alcohol_related: number;
+        year: number,
+        total: number | null
     }[];
 }
 
@@ -20,14 +14,22 @@ type AccidentData = {
 
 export function AccidentsByYear({data}: AccidentData){
     return (
-        <ResponsiveContainer width = "100%" minHeight={300}>
-        <BarChart data={data}>
-        <CartesianGrid />
-        <XAxis dataKey="year"/>
-        <YAxis/>
-        <Tooltip cursor={{fill: "transparent"}}></Tooltip>
-        <Bar dataKey="count" fill="red" barSize={30} />
-        </BarChart>
-        </ResponsiveContainer>
+        <div>
+            <ResponsiveContainer width = "100%" minHeight={300}>
+            <AreaChart data={data} margin={{top:10, right:30, left:0, bottom:0}}>
+            <defs>
+                <linearGradient id="fill" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#3f77d1" stopOpacity={0.8}/>
+                <stop offset="95%" stopColor="#3f77d1" stopOpacity={0}/>
+                </linearGradient>
+            </defs>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="year"/>
+            <YAxis type="number" domain={[0, (dataMax: any) => (Math.ceil(dataMax * 1.10))]}/>
+            <Tooltip cursor={false}/>
+            <Area dataKey="total" type="monotone" stroke="#3f77d1" fillOpacity={1} fill="url(#fill)"/>
+            </AreaChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
